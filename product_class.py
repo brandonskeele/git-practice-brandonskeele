2b7b274f3b866edd9bb1893612be765387263a2b
Brandon Keele <brandonskeele@gmail.com>
2024-01-30 13:05:33 -0700

    Created Product class

---
+++ product_class.py
@@ -0,0 +1,23 @@
+class Product:
+    def __init__(self, id, name, price):
+        self.id = id
+        self.name = name
+        self.price = price
+
+
+    def discount_price(self, percentage):
+        return self.price - (self.price * percentage)
+    
+
+    def change_price(self, new_price):
+        self.price = new_price
+
+
+toy = Product(12345, "Teddy Bear", 4)
+print(toy.id)
+print(toy.name)
+print(toy.price)
+
+print(toy.discount_price(0.20))
+toy.change_price(10.2)
+print(toy.price)
